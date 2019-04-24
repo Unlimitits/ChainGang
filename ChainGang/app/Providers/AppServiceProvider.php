74474fd9f3d80c2cb21331509d8d8ce5e5779688
chantalwiegand <38754673+chantalwiegand@users.noreply.github.com>
2019-04-24 14:43:32 +0200

    Make migration work

--- ChainGang/app/Providers/AppServiceProvider.php
+++ ChainGang/app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+
     /**
      * Register any application services.
      *
@@ -21,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
