7f0b8e95ae29e79428f1c26427dc8a2cc4d02324
chantalwiegand <38754673+chantalwiegand@users.noreply.github.com>
2019-05-05 13:31:48 +0200

    Eerste deel bootstrap homepage

---
+++ ChainGang/resources/views/Common templates/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <img class="navbar-brand" src="{{ asset('img/logo.png') }}">
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item padding-nav">
+                <a class="nav-link kleur-nav" href="#">Home</a>
+            </li>
+            <li class="nav-item padding-nav">
+                <a class="nav-link kleur-nav" href="#">Producten</a>
+            </li>
+            <li class="nav-item padding-nav">
+                <a class="nav-link kleur-nav" href="#">Contact</a>
+            </li>
+            <li class="nav-item padding-nav">
+                <a class="nav-link kleur-nav" href="#">Inloggen</a>
+            </li>
+            <img class="navbar-brand" src="{{ asset('img/avatar-placeholder.png') }}">
+        </ul>
+    </div>
+</nav>
