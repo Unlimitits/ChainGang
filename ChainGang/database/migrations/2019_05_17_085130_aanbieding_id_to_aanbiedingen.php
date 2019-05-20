cdc7d1911e765462e95c4d7799c418e4244fa5c5
NancyTang50 <nancy.tang@gc.graafschapcollege.nl>
2019-05-20 14:58:31 +0200

    project uploaded correct

---
+++ ChainGang/database/migrations/2019_05_17_085130_aanbieding_id_to_aanbiedingen.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AanbiedingIdToAanbiedingen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fiets', function (Blueprint $table) {
+            $table->foreign('aanbieding_id')->references('aanbieding_id')->on('aanbieding');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fiets', function (Blueprint $table) {
+            //
+        });
+    }
+}
